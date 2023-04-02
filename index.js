94ccfd435d8b6d5a19dc07291457e532778c02ba
rukia <ruqayashar1@gmail.com>
2023-04-02 07:02:48 +0300

    target sum

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  for(let i=0; i < array.length; i++){
+    const result1 = target-array[i];
+    for(let j= i+1; j< array.length; j++){
+      if(array[j] === result1) return true;
 
+    }
+  }
+  return false;
+  }
 /* 
   Write the Big O time complexity of your function here
-*/
-
+  linear  o(n) it  iterates through each element in an array
+time:o(n^2) - it has used nested iteration
 /* 
   Add your pseudocode here
+iterate through each array
+check wether the number and any other number in the array will result to our target.
+continue to iterate and check if the other numbers will add up to target.
+return true else false.
 */
 
 /*
@@ -29,6 +40,20 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  onsole.log("Expecting: true");
+  console.log("=>", hasTargetSum([20, 19, 4, 6, 30], 50));
+
+  console.log("");
+  onsole.log("Expecting: false");
+  console.log("=>", hasTargetSum([11, 8, 4,], 30));
+
+  console.log("");
+
+  onsole.log("Expecting: true");
+  console.log("=>", hasTargetSum([22, 19, 70, 6, 30], 100));
+
+  console.log("");
 }
 
 module.exports = hasTargetSum;
